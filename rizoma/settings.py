3241250b781e4af7297cb13dfbb13c81fddfdecf
jhrvo0 <jhrvo@cesar.school>
2024-10-15 21:18:17 -0300

    Update settings.py

--- rizoma/settings.py
+++ rizoma/settings.py
@@ -95,13 +95,6 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 

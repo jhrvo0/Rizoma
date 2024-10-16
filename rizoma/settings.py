3dc51232ab15c671d5d18003ac8530e40e675ff3
jhrvo0 <jhrvo@cesar.school>
2024-10-15 21:29:42 -0300

    Update settings.py

--- rizoma/settings.py
+++ rizoma/settings.py
@@ -19,7 +19,7 @@ if NOT_PROD:
         }
     }
 else:
-    SECRET_KEY = os.getenv('django-insecure-2rbhja+2^#%+jli-y_nwcgw7x(1)3oimb3(bqyhn5_#(2b=mxb')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
     ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
     CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')

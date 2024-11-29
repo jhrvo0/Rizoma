fbe828535e5c3a1dfaaa4d73d7eeedf75dd506f7
João Henrique <167437961+jhrvo0@users.noreply.github.com>
2024-11-28 21:46:00 -0300

    Update settings.py

--- rizoma/settings.py
+++ rizoma/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
-TARGET_ENV = os.getenv('TARGET_ENV', 'development')
+TARGET_ENV = os.getenv('TARGET_ENV')
 NOT_PROD = not TARGET_ENV.lower().startswith('prod')
 
 
